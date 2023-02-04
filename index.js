e9428cc2085329a533a2d8e5d3b87d68fdc1512f
RAHULSAGAR0123 <123389673+RAHULSAGAR0123@users.noreply.github.com>
2023-02-04 09:45:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose')
 const Post = require('./models/posts')
 const path = require("path")
 const cors = require("cors")
-const port = 8081 || process.env.PORT
-const cors = require("cors") 
-const port = 8090 || process.env.PORT
+const port = 8081 || process.env.PORT 
 const fileUpload = require("express-fileupload")
 const url = `mongodb+srv://RAHULSAGAR:RAHULSAGAR@cluster0.oyyx1sc.mongodb.net/?retryWrites=true&w=majority`
 const app = express()
@@ -95,13 +93,10 @@ app.get('/delete',async (req,res)=>{
 
 app.listen(port,()=>{
   console.log(`server is up at ${port}`)
-}
-
-
-
-
-
-
-app.listen(3008,()=>{
-  console.log("server is up and running at port 3008")
 })
+
+
+
+
+
+
